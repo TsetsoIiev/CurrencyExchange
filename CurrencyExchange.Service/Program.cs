591768eef764af9cc6fcb17efdcb48b2c50e15b2
tsetso <tsetsoiliev98@gmail.com>
2020-09-24 12:36:51 +0300

    add base reactjs project

--- CurrencyExchange.Service/Program.cs
+++ CurrencyExchange.Service/Program.cs
@@ -1,7 +1,4 @@
-﻿using CurrencyExchange.Service.Interfaces;
-using CurrencyExchange.Service.Services;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Hosting;
 
 namespace CurrencyExchange.Service
 {
